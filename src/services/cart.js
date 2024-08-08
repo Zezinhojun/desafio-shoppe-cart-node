4fd2cbacc2ce528bb6b7ab942edcf2777563b296
Junior Silva <zezinhojun@gmail.com>
2024-08-08 18:15:17 -0300

    fist commit

---
+++ src/services/cart.js
@@ -0,0 +1,47 @@
+async function addItem(userCart, item){
+    userCart.push(item);
+}
+
+async function calculateTotal(userCart){
+    console.log("Shopee Cart TOTAL IS:");
+    const result = userCart.reduce((total, item)=> total + item.subtotal(),0)
+    console.log(`🎁 Total: ${result}`)
+}
+
+async function deleteItem(userCart, name){
+    const index = userCart.findIndex((item)=> item.name === name);
+    if(index !== -1){
+        userCart.splice(index, 1);
+    }
+}
+
+async function removeItem(userCart, item){
+    const indexFound = userCart.findIndex((p)=> p.name === item.name)
+    if(indexFound == -1){
+        console.log("item não encontrado");
+        return;
+    }
+
+    if(userCart[indexFound].quantity > 1){
+        userCart[indexFound].quantity -= 1
+        return
+    }
+
+    if(userCart[indexFound].quantity == 1){
+        userCart.splice(indexFound, 1);
+        return
+    }
+
+    return console.log(indexFound)
+}
+
+async function displayCart(userCart){
+    console.log(`Shoppe cart list: `)
+    userCart.forEach((item, index)=>{
+        console.log(`${index + 1}. ${item.name} - R$ ${item.price} | ${item.quantity}x | Subtotal = ${item.subtotal()}`)
+    })
+}
+
+export {
+    addItem, calculateTotal,deleteItem,removeItem, displayCart
+}
